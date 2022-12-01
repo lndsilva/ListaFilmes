2231fb8bd1a791e646da76c20e873d6e91b0ec82
lndsilva <lndsilva@hotmail.com>
2022-12-01 20:24:45 -0300

    modelo de listas

--- app/src/main/java/brcom/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/brcom/etecia/listafilmes/MainActivity.java
@@ -3,12 +3,48 @@ package brcom.etecia.listafilmes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listaFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listaFilmes = findViewById(R.id.idlistaFilmes);
+
+        MyAdapter adapter = new MyAdapter();
+
+        //preparar para instanciar o adaptador
+        listaFilmes.setAdapter(adapter);
+    }
+    //criando uma inner class MyAdapter
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
